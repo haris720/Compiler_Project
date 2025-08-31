5739d42c34355661f1c764efdbf9de7b1d04217f
Saif Riaz <167188461+SaifRiaz3256@users.noreply.github.com>
2025-08-31 19:10:45 +0500

    Add Token structure definition in Token.h (Part A)

---
+++ Token.h
@@ -0,0 +1,13 @@
+//Token.h
+
+
+#pragma once
+#include <string>
+using namespace std;
+
+struct Token {
+    string type;
+    string value;
+
+    Token(string t, string v = "") : type(std::move(t)), value(std::move(v)) {}
+};
