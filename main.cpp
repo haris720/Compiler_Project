ec48966b76a6c7104b836b7107d0ade07756e811
asad7116 <asadmeh.9546@gmail.com>
2025-08-31 19:31:45 +0500

    Create main.cpp and printToken function

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//main.cpp:
+#include "Lexer.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printTokens(const vector<Token> &tokens) {
+    for (auto &tok : tokens) {
+        cout << "[" << tok.type;
+        if (!tok.value.empty())
+            cout << "(\"" << tok.value << "\")";
+        cout << "]";
+    }
+    cout << "\n";
+}
+int main(){
+
+  return 0;
+}
